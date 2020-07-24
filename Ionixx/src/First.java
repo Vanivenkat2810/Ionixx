0b15c755b74ad5af620b56c45fc30ac9a5f93546
Vanivenkat2810 <66713456+Vanivenkat2810@users.noreply.github.com>
2020-07-24 13:39:55 +0530

    Add files via upload

---
+++ Ionixx/src/First.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String a=null;
+		String b=null;
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			a=sc.next();
+			b=sc.next();
+			
+		if(a.equals("quit") || b.equals("quit"))
+		{
+			break;
+		}
+		else
+		{
+			continue;
+		}
+		}
+		
+	}
+	}
+
+
---
+++ Ionixx/src/Second.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		double b=a*a;
+		double c=Math.pow(a, a);
+		double d=b+c;
+		int answer=(int)d;
+		System.out.println(answer);
+		
+
+}
+}
